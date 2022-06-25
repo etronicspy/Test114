0a55d1352f35f9cdb4bd886a25de793f3def518c
etronicspy <etronicspy@gmail.com>
2022-06-25 13:29:40 +0300

    roter nicer commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ErrorComponent } from './error/error.component';
+import { ItemFormComponent } from './item-form/item-form.component';
+import { ItemComponent } from './item/item.component';
+import { ListComponent } from './list/list.component';
+import { MainComponent } from './main/main.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',component:MainComponent},
+  {path:'item-form',component:ItemFormComponent},
+  {path:'item',component:ItemComponent},
+  {path:'list',component:ListComponent},
+  {path:'**',component:ErrorComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,27 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainComponent } from './main/main.component';
+import { ItemComponent } from './item/item.component';
+import { ListComponent } from './list/list.component';
+import { ErrorComponent } from './error/error.component';
+import { ItemsService } from './services/item.service';
+import { ItemFormComponent } from './item-form/item-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    ItemComponent,
+    ListComponent,
+    ErrorComponent,
+    ItemFormComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [ItemsService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
